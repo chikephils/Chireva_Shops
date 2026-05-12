c2c678714639016770b5a95ed3d80aa9defbf671
Oreva Chike Okumor <chikephils@gmail.com>
2026-05-12 04:17:25 +0100

    fixed cors issues for mobile

--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -345,8 +345,8 @@ router.post(
       );
       const cookieOptions = {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        secure: true,
+        sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000,
         path: "/",
       };
